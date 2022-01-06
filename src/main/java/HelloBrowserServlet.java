5e51b21113a072e8adf5881809a7a57afc4ccf65
st1chka <kalinine03@gmail.com>
2022-01-06 19:12:21 +0300

    добавил аннотацию @WebServlet("/first_servlet")

--- src/main/java/HelloBrowserServlet.java
+++ src/main/java/HelloBrowserServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet
+@WebServlet("/first_servlet")
 public class HelloBrowserServlet extends HttpServlet {
     private static Logger logger = LoggerFactory.getLogger(HelloBrowserServlet.class);
 
