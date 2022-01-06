18d0de235b663e424334402a0d3ec497bcfbba08
st1chka <kalinine03@gmail.com>
2022-01-06 13:03:19 +0300

    add gitignore

---
+++ src/main/java/HelloBrowserServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloBrowserServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(HelloBrowserServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().printf("<html><body>");
+        for (int i = 0; i < 10; i++) {
+            resp.getWriter().printf("<h1>" + i + "</h1>");
+        }
+        resp.getWriter().printf("</html></body>");
+    }
+
+
+
+    @Override
+    public void destroy() {
+        logger.debug("Destroy");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        logger.debug("Init");
+    }
+}
